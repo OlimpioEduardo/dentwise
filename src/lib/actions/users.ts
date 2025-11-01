801938c56db79ce4b35dbcda9faa20b11e15f977
Olimpio Eduardo <kamuenhoeduardo@75gmail.com>
2025-11-01 12:21:58 +0100

    UserSync added

--- src/lib/actions/users.ts
+++ src/lib/actions/users.ts
@@ -1,28 +1,32 @@
-'use server'
+"use server";
 
-import {currentUser} from "@clerk/nextjs/server"
-import { prisma } from "../prisma"
+import { currentUser } from "@clerk/nextjs/server";
+import { prisma } from "../prisma";
 
-export async function syncUser (){
-    try {
-        const user = await currentUser()// usuario currente do Clerk
-        if (!user) return
 
-        const existingUser = await prisma.user.findUnique({where:{clerkId: user.id}})
+export async function syncUser() {
+  try {
+    const user = await currentUser();// usuario corrente do clerk
+    if (!user) return;
 
-       const dbUser = await prisma.user.create({
-            data:{
-                clerkId: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.emailAddresses[0].emailAddress,
-                phone: user.phoneNumbers[0]?.phoneNumber
-                
-            }
-        })
+    const existingUser = await prisma.user.findUnique({ where: { clerkId: user.id } });
+    if (existingUser) return existingUser;
 
-        return dbUser
-    } catch (error) {
-        console.log("Error in syncUser server action", error)
-    }
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        firstName: user.firstName,
+        lastName:  user.lastName,
+        email: user.emailAddresses[0].emailAddress,
+        phone: user.phoneNumbers[0]?.phoneNumber,
+      },
+
+      
+    });
+    console.log(user)
+
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser server action", error);
+  }
 }
